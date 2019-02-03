064c4cc9029d77548a755e9e80765091d854fdbd
Muttsu <22158218+Muttsu@users.noreply.github.com>
2019-02-02 19:12:31 -0500

    api update to ping tests

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,16 @@ const app = require('express')();
 app.use(require('cors')({origin: true}));
 const analyser = require('./analyser');
 
-app.get('/ping', (req, res) => {
-    res.send(analyser(['ping']));
+app.get('/ping', (req, response) => {
+    analyser(['ping']).then(res => {
+        response.send(res);
+        return;
+    }).catch(err => {
+        if (err) {
+            console.log('ERROR', err);
+            response.send(err);
+        }
+    })
 });
 
 // // Create and Deploy Your First Cloud Functions
