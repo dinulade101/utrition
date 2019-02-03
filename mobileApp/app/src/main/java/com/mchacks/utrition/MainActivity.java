39e0156bb14f6de6686275b61bbc150ecd4c3ac7
Dinula De Silva <dinulasd@gmail.com>
2019-02-02 22:16:25 -0500

    more updates

--- mobileApp/app/src/main/java/com/mchacks/utrition/MainActivity.java
+++ mobileApp/app/src/main/java/com/mchacks/utrition/MainActivity.java
@@ -1,15 +1,49 @@
 package com.mchacks.utrition;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    Uri imageUri;
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +60,54 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        textView = (TextView)findViewById(R.id.textView);
+
+        final int REQUEST_TAKE_PHOTO = 1;
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex){
+
+                    }
+                    if (photoFile != null){
+                        Uri photoUri = FileProvider.getUriForFile(getApplicationContext(), "com.mchacks.utrition.fileprovider", photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                        startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            switch (requestCode) {
+                case 0: {
+                    if (resultCode == RESULT_OK) {
+                        Log.d("myApp", "called");
+                        File file = new File(mCurrentPhotoPath);
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), Uri.fromFile(file));
+                        if (bitmap != null) {
+                            getTextFromBitmap(bitmap);
+                        }
+                    }
+                    break;
+                }
+            }
+        } catch (Exception error){
+            error.printStackTrace();
+        }
+
     }
 
     @Override
@@ -48,5 +130,76 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileNmae = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileNmae, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void getTextFromBitmap(Bitmap bitmap){
+
+//        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+//        if(!textRecognizer.isOperational()){
+//            Toast.makeText(getApplicationContext(), "Could not scan ingredients", Toast.LENGTH_SHORT).show();
+//        }
+//        else{
+//            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+//            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+//            StringBuilder stringBuilder = new StringBuilder();
+//            for (int i =0; i<items.size(); i++){
+//                TextBlock item = items.valueAt(i);
+//                stringBuilder.append(item.getValue());
+//                stringBuilder.append("\n");
+//            }
+//            textView.setText(stringBuilder.toString());
+//        }
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        detector.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                Log.e("myApp","success initial");
+                                processTextRecognitionResult(firebaseVisionText);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("myApp","faled");
+                                e.printStackTrace();
+                            }
+                        }
+                );
+
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts){
+        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0){
+            Log.e("myApp", "block size = 0");
+            return;
+        }
+        for (int i=0; i< blocks.size(); i++){
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j =0; j < lines.size(); j++){
+//                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+//                for (int k =0; k < elements.size(); k++){
+//
+//                }
+                String elem = lines.get(j).getText();
+                Log.d("MyApp", elem);
+            }
+        }
     }
 }
